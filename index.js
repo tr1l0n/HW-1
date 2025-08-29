4ab7b59ef45628d1df987faddcd018a3627b1e47
triloon <dimontopcik59@gmail.com>
2025-08-29 14:26:42 +0300

    homework1

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 let age = 15;
 console.log(age);
+
+
 let name = "Дмитро"
 console.log(name);
+
+
 let isStudent = true;
 console.log(isStudent);
+
+
 let myString = "Не бійся почати спочатку — цього разу ти почнеш розумнішим.";
 console.log(myString);
+
+
 let myNumber = 6;
 console.log(myNumber + 10);
+
+
 let myNull = null;
 console.log(myNull);
+
+
 let userName = prompt("Введіть ваше ім'я:");
 alert(`Ваше імя ${userName}`);
 console.log(typeof userName);
+
+
 let userAnswer = confirm("Ви дійсно хочете покинути сайт?");
 console.log(userAnswer);
+
+
 alert("Увага! Дія може бути небезпечною");
 confirm("Ви дійсно хочете продовжити?");
